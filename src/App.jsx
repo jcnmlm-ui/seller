a794763c9e225750d06aeb7c3cf4a60b7b4bb811
jcnmlm-ui <jcn.mlm@gmail.com>
2026-06-15 16:18:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Checkout         from './pages/customer/Checkout'
 import OrderConfirm     from './pages/customer/OrderConfirm'
 import OrderQuery       from './pages/customer/OrderQuery'
 import BoothDashboard   from './pages/booth/BoothDashboard'
+import CashierPage      from './pages/booth/CashierPage'
 import OrderDashboard   from './pages/admin/OrderDashboard'
 import ProductManage    from './pages/admin/ProductManage'
 import ReportsDashboard from './pages/admin/ReportsDashboard'
@@ -33,6 +34,7 @@ export default function App() {
 
               {/* 後台（需登入）*/}
               <Route path="/booth"           element={<ProtectedRoute><BoothDashboard /></ProtectedRoute>} />
+              <Route path="/cashier"         element={<ProtectedRoute><CashierPage /></ProtectedRoute>} />
               <Route path="/admin"           element={<ProtectedRoute><OrderDashboard /></ProtectedRoute>} />
               <Route path="/admin/products"  element={<ProtectedRoute><ProductManage /></ProtectedRoute>} />
               <Route path="/admin/reports"   element={<ProtectedRoute><ReportsDashboard /></ProtectedRoute>} />
